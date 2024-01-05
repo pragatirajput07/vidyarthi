634f816138a2fbf6e5927e3f0452b590b3f0c06a
pragatirajput07 <126343892+pragatirajput07@users.noreply.github.com>
2024-01-05 09:59:52 +0530

    added index.html

---
+++ backend.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+      <h2 style="color:#d7385e;">welcome to biet jhansi</h2>
+      <p><b>click below to download</b></p>
+      <a href="download.php?file=BFSproject_230112_213753" target="_biet">Download Now</a>
+    </center>
+      </body>
+</html>
